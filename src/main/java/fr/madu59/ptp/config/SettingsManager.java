11172e38b114301dd9ef9b7e9aefda1417510e0e
maDU59_ <matthias.ducourant@gmail.com>
2026-02-09 20:15:05 +0100

    Fixed boolean values in config

--- src/main/java/fr/madu59/ptp/config/SettingsManager.java
+++ src/main/java/fr/madu59/ptp/config/SettingsManager.java
@@ -297,6 +297,9 @@ public class SettingsManager {
         else if (defaultValue instanceof Float){
             return (T) Float.valueOf(loadedSettings.get(key));
         }
+        else if (defaultValue instanceof Boolean){
+            return (T) (Boolean) Boolean.parseBoolean(loadedSettings.get(key));
+        }
         else return null;
     }
 
