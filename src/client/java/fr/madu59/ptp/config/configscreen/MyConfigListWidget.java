349af13c1508689041f5de613090341f62611a2a
maDU59_ <matthias.ducourant@gmail.com>
2026-06-08 17:38:51 +0200

    Fixed merge

--- src/client/java/fr/madu59/ptp/config/configscreen/MyConfigListWidget.java
+++ src/client/java/fr/madu59/ptp/config/configscreen/MyConfigListWidget.java
@@ -11,7 +11,7 @@ import fr.madu59.ptp.config.configscreen.entries.builders.SliderBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
