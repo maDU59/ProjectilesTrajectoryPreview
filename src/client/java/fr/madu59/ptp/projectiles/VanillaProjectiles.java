0af13228fbc0e961973f627bdf1d3f528ab2f215
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-19 15:41:38 +1000

    perf: early return for uncharged or unpulled vanilla projectiles

--- src/client/java/fr/madu59/ptp/projectiles/VanillaProjectiles.java
+++ src/client/java/fr/madu59/ptp/projectiles/VanillaProjectiles.java
@@ -30,28 +30,29 @@ public class VanillaProjectiles {
     private static double DEFAULT_WATER_DRAG = 0.6;
 
     public static void bowTrajectory(ItemStack itemStack, Player player, List<ProjectileData> out){
-        float tickProgress = PtpClient.getTickProgress();
-
-        Vec3 position = TrajectoryUtils.getAimPos(player, tickProgress);
-            
         int useTicks = player.getTicksUsingItem();
         float pull = BowItem.getPowerForTime(useTicks);
+        if (pull < 0.1F) return;
 
+        float tickProgress = PtpClient.getTickProgress();
+        Vec3 position = TrajectoryUtils.getAimPos(player, tickProgress);
         Vec3 vel = TrajectoryUtils.getViewVector(player, tickProgress).scale(3.0 * pull);
         Vec3 offset = new Vec3(0.2, -0.06, 0.2);
 
-        if(pull >= 0.1) out.add(new ProjectileData(DEFAULT_GRAVITY, DEFAULT_DRAG, vel, offset, position, false, DEFAULT_WATER_DRAG, TrajectoryUtils.ORDER_PDG, false));
+        out.add(new ProjectileData(DEFAULT_GRAVITY, DEFAULT_DRAG, vel, offset, position, false, DEFAULT_WATER_DRAG, TrajectoryUtils.ORDER_PDG, false));
     }
 
     public static void crossbowTrajectory(ItemStack itemStack, Player player, List<ProjectileData> out){
+        if (!CrossbowItem.isCharged(itemStack)) return;
+
         float tickProgress = PtpClient.getTickProgress();
 
         double gravity = DEFAULT_GRAVITY;
         double waterDrag = DEFAULT_WATER_DRAG;
 
         Vec3 position = TrajectoryUtils.getAimPos(player, tickProgress);
-
-        Vec3 vel = TrajectoryUtils.getViewVector(player, tickProgress).scale(3.15);
+        Vec3 viewVector = TrajectoryUtils.getViewVector(player, tickProgress);
+        Vec3 vel = viewVector.scale(3.15);
         Vec3 offset = new Vec3(0, -0.06, 0.03);
 
         ChargedProjectiles chargedProjectilesComponent = itemStack.get(DataComponents.CHARGED_PROJECTILES);
@@ -59,7 +60,7 @@ public class VanillaProjectiles {
         if(chargedProjectilesComponent != null){
             for (ItemStackTemplate projectile : chargedProjectilesComponent.items()) {
                 if (projectile.is(Items.FIREWORK_ROCKET)) {
-                    vel = TrajectoryUtils.getViewVector(player, tickProgress).scale(1.6F);
+                    vel = viewVector.scale(1.6F);
                     gravity = 0;
                     waterDrag = DEFAULT_DRAG;
                 } else if (projectile.item().value() instanceof ArrowItem) {
@@ -68,33 +69,29 @@ public class VanillaProjectiles {
             }
         }
 
-        if(CrossbowItem.isCharged(itemStack)) {
-            out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
-            if (ItemUtils.hasEnchantment(itemStack, Enchantments.MULTISHOT)){
-                float angleOffset = 10f;
-                Vec3 vel1 = vel.yRot((float) Math.toRadians(angleOffset));
-                Vec3 vel2 = vel.yRot((float) Math.toRadians(-angleOffset));
-                out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel1, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
-                out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel2, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
-            }
+        out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
+        if (ItemUtils.hasEnchantment(itemStack, Enchantments.MULTISHOT)){
+            float angleOffset = 10f;
+            Vec3 vel1 = vel.yRot((float) Math.toRadians(angleOffset));
+            Vec3 vel2 = vel.yRot((float) Math.toRadians(-angleOffset));
+            out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel1, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
+            out.add(new ProjectileData(gravity, DEFAULT_DRAG, vel2, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
         }
     }
 
     public static void tridentTrajectory(ItemStack itemStack, Player player, List<ProjectileData> out){
-        float tickProgress = PtpClient.getTickProgress();
-
-        double waterDrag = 0.99;
-
         int useTicks = player.getTicksUsingItem();
+        if (useTicks < TridentItem.THROW_THRESHOLD_TIME || ItemUtils.hasEnchantment(itemStack, Enchantments.RIPTIDE)) {
+            return;
+        }
 
+        float tickProgress = PtpClient.getTickProgress();
+        double waterDrag = 0.99;
         Vec3 position = TrajectoryUtils.getAimPos(player, tickProgress);
-
         Vec3 vel = TrajectoryUtils.getViewVector(player, tickProgress).scale(TridentItem.PROJECTILE_SHOOT_POWER);
         Vec3 offset = new Vec3(0.2, 0.1, 0.2);
 
-        if(useTicks >= TridentItem.THROW_THRESHOLD_TIME && !ItemUtils.hasEnchantment(itemStack, Enchantments.RIPTIDE)){
-            out.add(new ProjectileData(DEFAULT_GRAVITY, DEFAULT_DRAG, vel, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
-        }
+        out.add(new ProjectileData(DEFAULT_GRAVITY, DEFAULT_DRAG, vel, offset, position, false, waterDrag, TrajectoryUtils.ORDER_PDG, false));
     }
 
     public static void snowballTrajectory(ItemStack itemStack, Player player, List<ProjectileData> out){
